c3caf4ac0fd6f87d656934e1df3daad3d0fe6142
toddyneto9-lgtm <toddyneto9@gmail.com>
2026-07-15 00:13:49 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+function entrar() {
+    let usuario = document.querySelector("input[type='text']").value;
 
+    if (usuario === "") {
+        alert("Digite seu usuário!");
+    } else {
+        window.location.href = "dashboard.html";
+    }
+}
